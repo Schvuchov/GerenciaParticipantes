12d75217fedd6327a7d76b387264991b27afb228
Barbara Schvuchov <schvuchov.k@gmail.com>
2024-04-02 22:53:39 -0300

    layout

---
+++ pass-in-web/src/components/header.tsx
@@ -0,0 +1,15 @@
+import nlwIcon from '../assets/nlw-icon.svg'
+
+export function Header() {
+     return(
+        <div className='flex items-center gap-5 py-2'>
+            <img src={nlwIcon} />
+
+            <nav className='flex items-center gap-5'>
+                <a href="" className='font-medium text-sm text-zinc-300'>Eventos</a>
+                <a href="" className='font-medium text-sm'>Participantes</a>
+            </nav>
+        </div>
+     )
+    
+}
