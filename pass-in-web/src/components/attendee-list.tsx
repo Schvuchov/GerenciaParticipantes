606a812a4def410219b7a8f37300a36d51e37d70
Barbara Schvuchov <schvuchov.k@gmail.com>
2024-04-04 16:29:46 -0300

    finalizando

--- pass-in-web/src/components/attendee-list.tsx
+++ pass-in-web/src/components/attendee-list.tsx
@@ -7,7 +7,7 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
 dayjs.locale('pt-br')
 
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 
 //components
 import { IconButton } from './icon-button'
@@ -15,34 +15,104 @@ import { Table } from './table/table'
 import { TableHeader } from './table/table-header'
 import { TableCell } from './table/table-cell'
 import { TableRow } from './table/table-row'
-import { attendees } from '../data/attwndees'
 
 
+interface Attendee {
+    id: string
+    name: string
+    email: string
+    createdAt: string
+    checkedInAt: string | null
+}
 
 export function AttendeeList(){
-    const [search, setSearch] = useState('')
-    const [page, setPage] = useState(1)
+    const [search, setSearch] = useState(() => {
+        const url = new URL(window.location.toString())
 
-    const totalPages = Math.ceil(attendees.length / 10)
+        //verifica se na url tem algum parametro de busca
+        if (url.searchParams.has('search')) {
+            //search tambem pode ser nulo e nesse caso ( ?? "") queremos que retorne como se não tivesse
+            return url.searchParams.get('search') ?? ''
+        }
+
+        return ''
+    })
+
+    const [page, setPage] = useState(() => {
+        const url = new URL(window.location.toString())
+
+        //verifica se na url tem algum parametro indicando alguma página
+        if (url.searchParams.has('page')) {
+            return Number(url.searchParams.get('page'))
+        }
+
+        return 1
+    })
+
+    const [attendees, setAttendees] = useState<Attendee[]>([])
+    const [total, setTotal] = useState(0)
+
+    const totalPages = Math.ceil(total / 10)
+
+    useEffect(() => {
+        const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+        url.searchParams.set('pageIndex', String(page - 1))
+        if (search.length > 0){
+            url.searchParams.set('query', search)
+        }
+        
+        
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                setAttendees(data.attendees)
+                setTotal(data.total)
+            })
+    }, [page, search])
+
+
+    //queremos que a url salve alguns params, por exemplo quando compartilha o link estando em uma certa pag e/ou então tendo feito alguma busca
+
+    function setCurrentPage(page: number) {
+        const url = new URL(window.location.toString())
+
+        url.searchParams.set('page', String(page))
+
+        //pushState altera o endereço do navegador sem fazer o recarregamento da página
+        window.history.pushState({}, "", url)
+
+        setPage(page)
+    }
+
+    function setCurrentSearch(search: string) {
+        const url = new URL(window.location.toString())
+
+        url.searchParams.set('search', search)
+
+        window.history.pushState({}, "", url)
+
+        setSearch(search)
+    }
 
     function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
-        setSearch(event.target.value)
+        setCurrentSearch(event.target.value)
+        setCurrentPage(1)
     }
 
     function goToFirstPage() {
-        setPage(1)
+        setCurrentPage(1)
     }
 
     function goToPreviousPage(){
-        setPage(page - 1)
+        setCurrentPage(page - 1)
     }
 
     function goToNextPage(){
-        setPage(page + 1)
+        setCurrentPage(page + 1)
     }
 
     function goToLastPage() {
-        setPage(totalPages)
+        setCurrentPage(totalPages)
     }
 
 
@@ -53,7 +123,11 @@ export function AttendeeList(){
 
                 <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
                     <Search className='size-4 text-emerald-300'/>
-                    <input onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participante..." />
+                    <input 
+                        onChange={onSearchInputChanged} 
+                        value={search}
+                        className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0" 
+                        placeholder="Buscar participante..." />
                 </div>
             </div>
 
@@ -71,7 +145,7 @@ export function AttendeeList(){
                     </tr>
                 </thead>
                 <tbody>
-                    {attendees.slice((page -1) * 10, page * 10).map((attendee) => {
+                    {attendees.map((attendee) => {
                         return(
                             <TableRow key={attendee.id} className='border-b border-white/10 hover:bg-white/5'>
                                 <TableCell>
@@ -81,11 +155,15 @@ export function AttendeeList(){
                                 <TableCell>
                                     <div className='flex flex-col gap-1'>
                                         <span className='font-semibold text-white'>{attendee.name}</span>
-                                        <span>{attendee.email}</span>
+                                        <span>{attendee.email.toLocaleLowerCase()}</span>
                                     </div>
                                 </TableCell>
                                 <TableCell>{ dayjs().to(attendee.createdAt)}</TableCell>
-                                <TableCell>{ dayjs().to(attendee.checkedInAt) }</TableCell>
+                                <TableCell>
+                                    { attendee.checkedInAt === null 
+                                        ? <span className='text-zinc-400'>Não fez check-in</span> 
+                                        : dayjs().to(attendee.checkedInAt) }
+                                </TableCell>
                                 <TableCell>
                                     <IconButton transparent>
                                         <MoreHorizontal className='size-4'/>
@@ -98,7 +176,7 @@ export function AttendeeList(){
                 <tfoot>
                     <tr>
                         <TableCell  colSpan={3}>
-                            Mostrando 10 de {attendees.length} itens
+                            Mostrando {attendees.length} de {total} itens
                         </TableCell>
 
                         <td className='py-3 px-4 text-sm text-zinc-300 text-right' colSpan={3}>
